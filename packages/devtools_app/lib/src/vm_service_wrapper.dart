c3f63dfc6004368ad72411328e74a98485e6c398
Jacob Richman <jacob314@gmail.com>
2021-01-29 09:46:47 -0800

    Fix bug where offset and count values were not passed through.

--- packages/devtools_app/lib/src/vm_service_wrapper.dart
+++ packages/devtools_app/lib/src/vm_service_wrapper.dart
@@ -381,7 +381,14 @@ class VmServiceWrapper implements VmService {
     int offset,
     int count,
   }) {
-    return trackFuture('getObject', _vmService.getObject(isolateId, objectId));
+    return trackFuture(
+        'getObject',
+        _vmService.getObject(
+          isolateId,
+          objectId,
+          offset: offset,
+          count: count,
+        ));
   }
 
   @override
